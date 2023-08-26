4c53c179542b13679654cbd8690f0ab521e18bbd
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2023-08-26 19:06:25 +0330

    fix course filtering

--- course/views.py
+++ course/views.py
@@ -1,10 +1,8 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.views import APIView
 from .models import *
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
+from django.db.models import Q
 # Create your views here.
 
 
@@ -32,7 +30,10 @@ class UserCourseView(ListAPIView):
 class FilterCourse(ListAPIView):
     serializer_class = CourseSerializer
     def get_queryset(self):
-        prof = self.request.query_params.get('teachers')
-        if prof == None:
+        prof = self.request.query_params.getlist("teachers")
+        if not prof:
             return Course.objects.all()
-        return Course.objects.filter(teacher__fullname=prof)
+        q_object = Q()
+        for prof_name in prof:
+            q_object |= Q(teacher__fullname=prof_name)
+        return Course.objects.filter(q_object)
